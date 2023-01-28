060f675b528d43b9d705f88c02d823ff688dd813
casainho <casainho@gmail.com>
2023-01-28 16:35:26 +0000

    WIP

--- firmware/main.py
+++ firmware/main.py
@@ -1,10 +1,13 @@
 import board
 import buttons
 import display_sh1106
-import displayio
+import ebike_board
+import ebike_data
 import time
+import displayio
 from adafruit_display_text import label
-from fruity_menu.menu import Menu
+import terminalio
+
 
 buttons = buttons.Buttons(
         board.IO33, # POWER
@@ -20,14 +23,130 @@ displayObject = display_sh1106.Display(
         1000000) # spi clock frequency
 display = displayObject.display
 
+ebike_data = ebike_data.EBike()
+ebike = ebike_board.EBikeBoard(
+    board.IO18, # UART TX pin that connect to display UART RX pin
+    board.IO17, # UART RX pin that connect to display UART TX pin
+    ebike_data) # EBike data object to hold the EBike data
+
+DISPLAY_WIDTH = 64  
+DISPLAY_HEIGHT = 128
+TEXT = "0"
+
+def motor_power_round(motor_power):
+
+    if motor_power < 10:
+        motor_power = 0
+    elif motor_power < 20:
+        motor_power /= 2.5
+        motor_power *= 2.5
+    elif motor_power < 100:
+        motor_power /= 5
+        motor_power *= 5
+    elif motor_power < 250:
+        motor_power /= 10
+        motor_power *= 10
+    elif motor_power < 500:
+        motor_power /= 20
+        motor_power *= 20
+    elif motor_power < 500:
+        motor_power /= 25
+        motor_power *= 25
+
+    return int(motor_power)
+
+assist_level_area = label.Label(terminalio.FONT, text=TEXT)
+assist_level_area.anchor_point = (0.0, 0.0)
+assist_level_area.anchored_position = (4, 0)
+assist_level_area.scale = 2
+
+battery_voltage_area = label.Label(terminalio.FONT, text=TEXT)
+battery_voltage_area.anchor_point = (0.0, 0.0)
+battery_voltage_area.anchored_position = (34, 0)
+battery_voltage_area.scale = 1
+
+label_x = 1
+label_y = 22
+label_y_offset = 32
+label_1 = label.Label(terminalio.FONT, text=TEXT)
+label_1.anchor_point = (0.0, 0.0)
+label_1.anchored_position = (label_x, label_y)
+label_1.scale = 2
+
+label_y += label_y_offset
+label_2 = label.Label(terminalio.FONT, text=TEXT)
+label_2.anchor_point = (0.0, 0.0)
+label_2.anchored_position = (label_x, label_y)
+label_2.scale = 2
+
+label_y += label_y_offset
+label_3 = label.Label(terminalio.FONT, text=TEXT)
+label_3.anchor_point = (0.0, 0.0)
+label_3.anchored_position = (label_x, label_y)
+label_3.scale = 2
+
+text_group = displayio.Group()
+text_group.append(assist_level_area)
+text_group.append(battery_voltage_area)
+text_group.append(label_1)
+text_group.append(label_2)
+text_group.append(label_3)
+
+display.show(text_group)
+
+assist_level = 0
+assist_level_state = 0
+now = time.monotonic()
+assist_level_time_previous = now
+display_time_previous = now
+
+battery_voltage_previous = 9999
+motor_power_previous = 9999
+motor_temperature_sensor_x10_previous = 9999
+vesc_temperature_x10_previous = 9999
+
 while True:
-    power = "enable" if buttons.power else "disable"
-    up = "enable" if buttons.up else "disable"
-    down = "enable" if buttons.down else "disable"
+    now = time.monotonic()
+    if (now - display_time_previous) > 1.0:
+        display_time_previous = now
 
-    print( )
-    print(f"POWER button: {power}")
-    print(f"UP button: {up}")
-    print(f"DOWN button: {down}")
+        if battery_voltage_previous != ebike_data.battery_voltage:
+            battery_voltage_previous = ebike_data.battery_voltage
+            battery_voltage_area.text = str(f"{ebike_data.battery_voltage:2.1f}v")
 
-    time.sleep(1)
+        if motor_power_previous != ebike_data.motor_power:
+            motor_power_previous = ebike_data.motor_power
+            motor_power = motor_power_round(ebike_data.motor_power)
+            label_1.text = str(f"{motor_power:5}")
+        
+        if motor_temperature_sensor_x10_previous != ebike_data.motor_temperature_sensor_x10:
+            motor_temperature_sensor_x10_previous = ebike_data.motor_temperature_sensor_x10  
+            label_2.text = str(f"{(ebike_data.motor_temperature_sensor_x10 / 10.0): 2.1f}")
+
+        if vesc_temperature_x10_previous != ebike_data.vesc_temperature_x10:
+            vesc_temperature_x10_previous = ebike_data.vesc_temperature_x10  
+            label_3.text = str(f"{(ebike_data.vesc_temperature_x10 / 10.0): 2.1f}")    
+
+    ebike.process_data()
+
+    now = time.monotonic()
+    if (now - assist_level_time_previous) > 0.05:
+        assist_level_time_previous = now
+
+        # change assist level
+        if assist_level_state == 1 and not buttons.up:
+            assist_level_state = 0
+
+        if assist_level_state == 2 and not buttons.down:
+            assist_level_state = 0
+
+        if assist_level < 20 and buttons.up and assist_level_state == 0:
+            assist_level_state = 1
+            assist_level += 1
+
+        if assist_level > 0 and buttons.down and assist_level_state == 0:
+            assist_level_state = 2
+            assist_level -= 1
+
+        ebike_data.assist_level = assist_level
+        assist_level_area.text = str(assist_level)
